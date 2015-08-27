1800d534888cd5d22250b217add527d7694e8fe6
Julien Beaudaux <julien.beaudaux@schiller.fr>
2015-08-27 15:37:47 +0200

    flashcard listing
    Signed-off-by: Julien Beaudaux <julien.beaudaux@schiller.fr>

--- mnemobop.cpp
+++ mnemobop.cpp
@@ -1,30 +1,88 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <dirent.h>
+#include <vector>
 using namespace std;
 
-int main()
+
+bool listSelection(void)
 {
-    string const inFileName("./lists/TestList.data");
+    int i;
+    DIR *dir;
+    struct dirent *ent;
+    std::string filename;
+
+    std::vector<string> availableLists;
+    availableLists.clear();
+
+    std::ifstream infile;
     std::string myline;
 
-    //vector<vector<int> > wordList;
-
-    std::ifstream infile(inFileName.c_str());
-
-    if(infile)
+    if((dir = opendir("./lists/")) != NULL)
     {
+        // List all files in directory
+        printf("Available flash-card lists :\n");
+        while ((ent = readdir (dir)) != NULL)
+        {
+            if(ent->d_name[0] != '.')
+            {
+                availableLists.push_back(string("lists/") + string(ent->d_name));
+            }
+        }
+        closedir (dir);
+
+        for(i=0; i<availableLists.size(); i++)
+        {
+            cout << "\t" << i+1 << ") " << availableLists[i] << endl;
+        }
+
+        do
+        {
+            cin >> i;
+
+            if(i<=availableLists.size() && i>0)
+            {
+                infile.open(availableLists[i-1].c_str());
+
+                if(infile.is_open() == true)
+                {
+                    cout << "List opened : " << availableLists[i-1].c_str() << endl;
+                }
+                else
+                {
+                    cout << "Failed to open list : " << availableLists[i-1].c_str() << endl;
+                }
+            }
+            else
+            {
+                cout << "Bad file ID entered : " << i << endl;
+            }
+        }while(infile.is_open() == false);
+
         while(getline(infile, myline))
         {
             cout << myline << endl;
         }
+        
+        infile.close();
+
+        return true;
     }
     else
     {
-        cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
-    }
+        /// Failed to open directory
+        //perror ("");
+        cout << "ERROR: Failed to open lists directory" << endl;
 
-    infile.close();
+        return false;
+    }
+}
+
+
+int main()
+{
+    listSelection();
 
     return 0;
 }
